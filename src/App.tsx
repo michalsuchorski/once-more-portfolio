d1bd45c6300b0207777c2bdc1530e0bf215e87a9
Michal Suchorski <michal.suchorski7@gmail.com>
2024-06-28 13:44:22 +0200

    phone support

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,18 @@ import {
   Image,
 } from "@chakra-ui/react";
 import { FaInstagram, FaFacebookF } from "react-icons/fa";
-import OnceMore from './imgs/oncemore_napis.png'
+import OnceMore from "./imgs/oncemore_napis.png";
 
 function App() {
   return (
     <Flex backgroundColor="red" w="100%" h="100vh" alignItems="center">
-      <VStack backgroundColor="white" p={16} >
+      <VStack backgroundColor="white" p={{ base: 4, md: 10, lg: 16 }}>
         <VStack alignItems="flex-start">
-          <Image src={OnceMore} />
-          <Text maxW='40%' h='auto'>
+          <Image
+            src={OnceMore}
+            width={{ base: "400px", md: "500px", lg: "700px" }}
+          />
+          <Text w={{ base: "100%", md: "40%", lg: "40%" }} h="auto">
             BE-NA IS A BRANDING STUDIO LED BY CREATIVE DIRECTOR, BINA ALTERA.
             BE-NA WORKS WITH HEART-CENTERED CHANGE MAKERS AND PURPOSE-LED
             ORIGINAL THINKERS DREAMERS, OVERACHIEVERS AND MISFITS WHO WANT TO
@@ -26,17 +29,17 @@ function App() {
             DESIGN. IF YOU HAVE A VISION, WE CAN BRING IT TO LIFE.
           </Text>
         </VStack>
-        <HStack
+        <Flex
+          direction={{ base: "column", md: "row", lg: "row" }}
           w="100%"
           textStyle="uppercase"
           color="red.500"
           fontSize="x-large"
           mt={10}
-          spacing={5}
           fontWeight="500"
           letterSpacing="1.5px"
         >
-          <HStack>
+          <HStack mr={8}>
             <IconButton
               colorScheme="red"
               isRound={true}
@@ -54,12 +57,20 @@ function App() {
               icon={<FaFacebookF />}
             />
           </HStack>
-          <Link>HOME</Link>
-          <Link>BEE</Link>
-          <Link>MORE</Link>
-          <Link>DODATKI</Link>
-          <Button size='lg'>KONTAKT</Button>
-        </HStack>
+          <Flex
+            alignItems={{ base: "flex-start", md: "center", lg: "center" }}
+            w="100%"
+            gap={8}
+            flexDirection={{ base: "column", md: "row", lg: "row" }}
+            mt={{ base: 8, md: '0px', lg: '0px'}}
+          >
+            <Link>HOME</Link>
+            <Link>BEE</Link>
+            <Link>MORE</Link>
+            <Link>DODATKI</Link>
+            <Button size="lg">KONTAKT</Button>
+          </Flex>
+        </Flex>
       </VStack>
     </Flex>
   );
