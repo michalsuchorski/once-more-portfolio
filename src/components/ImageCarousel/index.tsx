5f5cbea102315b692e4fe7eff551e0134af8100a
Michal Suchorski <michal.suchorski7@gmail.com>
2024-07-15 12:03:23 +0200

    image carousel bug fixes and changes, images description formatting fix

--- src/components/ImageCarousel/index.tsx
+++ src/components/ImageCarousel/index.tsx
@@ -14,9 +14,15 @@ import {
   IconButton,
   Box,
 } from "@chakra-ui/react";
-import img1 from "../../components/BackgroundImages/p6.jpg";
-import img4 from "../../components/BackgroundImages/p1.jpeg";
-import img5 from "../../components/BackgroundImages/p3.jpg";
+import img1 from "../../imgs/home-img/bee-min.png";
+import img2 from "../../imgs/home-img/freedom-min.png";
+import img3 from "../../imgs/home-img/love-min.png";
+import img4 from "../../imgs/home-img/moonlight-min.png";
+import img5 from "../../imgs/home-img/morewildlife-min.png";
+import img6 from "../../imgs/home-img/shine-min.png";
+import img7 from "../../imgs/home-img/trees-min.png";
+import img8 from "../../imgs/home-img/water-min.png";
+
 import { ArrowForwardIcon, ArrowBackIcon } from "@chakra-ui/icons";
 
 interface ImageData {
@@ -25,19 +31,46 @@ interface ImageData {
   description: string;
 }
 
+const bee = 'Pszczoły pełnią kluczową rolę w całym systemie ekologicznym. Zapylają około 80% roślin. Bez pszczół, rozwój i wzrost roślin nie jest możliwy. Trudne warunki w środowisku naturalnym, są dla nich dużym zagrożeniem i przyczyniają się do ich wymierania. Gdyby zabrakło pszczoły miodnej, nie było by, większości owoców i warzyw. Istnieje przekonanie, że od owadów zapylających zależy gospodarka żywnościowa, a więc i sytuacja społeczna i ekonomiczna na świecie.'
+
 const images: ImageData[] = [
   {
     src: img1,
-    alt: "Image 1",
-    description: "Description for Image 1",
+    alt: "bee",
+    description: bee,
   },
   {
-    src: img4,
+    src: img2,
     alt: "Image 2",
     description: "Description for Image 2",
   },
   {
+    src: img3,
+    alt: "Image 3",
+    description: "Description for Image 3",
+  },
+  {
+    src: img4,
+    alt: "Image 1",
+    description: "Description for Image 1",
+  },
+  {
     src: img5,
+    alt: "Image 2",
+    description: "Description for Image 2",
+  },
+  {
+    src: img6,
+    alt: "Image 3",
+    description: "Description for Image 3",
+  },
+  {
+    src: img7,
+    alt: "Image 2",
+    description: "Description for Image 2",
+  },
+  {
+    src: img8,
     alt: "Image 3",
     description: "Description for Image 3",
   },
@@ -92,36 +125,21 @@ const Carousel: React.FC = () => {
   const settings = {
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
+    centerMode: true,
+    variableWidth: true,
     nextArrow: <NextArrow />,
     prevArrow: <PrevArrow />,
     responsive: [
       {
-        breakpoint: 1054,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-        },
-      },
-      {
-        breakpoint: 1036,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-          infinite: true,
-        },
-      },
-      {
-        breakpoint: 530,
+        breakpoint: 708,
         settings: {
           slidesToShow: 1,
           slidesToScroll: 1,
           infinite: true,
+          centerMode: false, 
+          variableWidth: false,
         },
-      },
+      }
     ],
   };
 
@@ -141,8 +159,21 @@ const Carousel: React.FC = () => {
                 key={image.src}
                 onClick={() => handleImageClick(image)}
                 cursor="pointer"
+                alignItems='center'
+                overflow='hidden'
+                justifyContent='center'
+                w='auto'
+                h='auto'
               >
-                <Image src={image.src} alt={image.alt} maxW="100%" />
+                <Image
+                  src={image.src}
+                  alt={image.alt}
+                  w='auto'
+                  h='auto'
+                  maxH='80vh'
+                  maxW="100%"
+                  objectFit="contain"
+                />
               </Flex>
             ))}
           </Slider>
@@ -159,7 +190,7 @@ const Carousel: React.FC = () => {
           justifyContent="center"
           alignItems="center"
           overflow="hidden"
-          maxW='none'
+          maxW="none"
         >
           <ModalHeader>{selectedImage?.alt}</ModalHeader>
           <ModalCloseButton />
@@ -173,11 +204,11 @@ const Carousel: React.FC = () => {
             <Image
               src={selectedImage?.src}
               alt={selectedImage?.alt}
-              maxW="90%"
-              maxH="90vh"
+              borderRadius={4}
+              maxH="70vh"
               objectFit="contain"
             />
-            <Text mt={2} py={4}>
+            <Text py={4} px={{ base: 2, md: 8, lg: 16}} textAlign='center' maxW='1440px'>
               {selectedImage?.description}
             </Text>
           </ModalBody>
