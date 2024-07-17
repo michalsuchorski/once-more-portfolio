b0ba0dde1e3bbbc3fae7cdd52e7231814d06acf7
Michal Suchorski <michal.suchorski7@gmail.com>
2024-07-17 12:46:12 +0200

    galleryItem fix

--- src/components/GalleryItem/index.tsx
+++ src/components/GalleryItem/index.tsx
@@ -1,11 +1,21 @@
-import { Box, GridItem, Image, Modal, ModalBody, ModalCloseButton, ModalContent, ModalOverlay, useDisclosure } from "@chakra-ui/react";
+import {
+  Box,
+  GridItem,
+  Image,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
 import React from "react";
 import { NextArrow, PrevArrow } from "../CarouselArrows";
 import Slider from "react-slick";
 import { ImageData } from "./types";
 
 interface GalleryItemProps {
-    images: ImageData[]
+  images: ImageData[];
 }
 
 export const GalleryItem: React.FC<GalleryItemProps> = ({ images }) => {
@@ -24,76 +34,79 @@ export const GalleryItem: React.FC<GalleryItemProps> = ({ images }) => {
     slidesToShow: 3,
     slidesToScroll: 3,
     responsive: [
-        {
-            breakpoint: 1025,
-            settings: {
-                dots: true,
-                slidesToShow: 2,
-                slidesToScroll: 2,
-                infinite: true,
-                nextArrow: <NextArrow />,
-                prevArrow: <PrevArrow />,
-            },  
+      {
+        breakpoint: 1025,
+        settings: {
+          dots: true,
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          nextArrow: <NextArrow />,
+          prevArrow: <PrevArrow />,
         },
-        {
-            breakpoint: 644,
-            settings: {
-                dots: true,
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true,
-                nextArrow: <NextArrow />,
-                prevArrow: <PrevArrow />,
-            },
-        }
-    ]
+      },
+      {
+        breakpoint: 644,
+        settings: {
+          dots: true,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          nextArrow: <NextArrow />,
+          prevArrow: <PrevArrow />,
+        },
+      },
+    ],
   };
 
   return (
     <>
-         {images.map((image) => {
-            return (
-              <GridItem
-                w="100%"
-                cursor="pointer"
-                key={image.src}
-                onClick={() => handleImageClick(image)}
-              >
-                <Image src={image.src} />
-                <Modal isOpen={isOpen} onClose={onClose} isCentered>
-                  <ModalOverlay />
-                  <ModalContent height={{base: '85vh', md: '85vh', lg: '100vh'}} w="100vw" maxW="none">
-                    <ModalCloseButton />
-                    <ModalBody
-                      display="flex"
-                      flexDirection="column"
-                      justifyContent="center"
-                      alignItems="center"
-                      p={0}
-                    >
-                      <Box w="full">
-                        <Slider {...settings}>
-                          {selectedImage?.subImgs.map((subImage) => {
-                            return (
-                              <Image
-                                borderRadius='md'
-                                p={{ base: 2, md: 2, lg: 1}}
-                                src={subImage}
-                                w="auto"
-                                h="auto"
-                                maxH="100vh"
-                                maxW="100%"
-                              />
-                            );
-                          })}
-                        </Slider>
-                      </Box>
-                    </ModalBody>
-                  </ModalContent>
-                </Modal>
-              </GridItem>
-            );
-          })}
+      {images.map((image) => {
+        return (
+          <GridItem
+            w="100%"
+            cursor="pointer"
+            key={image.src}
+            onClick={() => handleImageClick(image)}
+          >
+            <Image src={image.src} />
+          </GridItem>
+        );
+      })}
+
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
+        <ModalOverlay />
+        <ModalContent
+          height={{ base: "85vh", md: "85vh", lg: "100vh" }}
+          w="100vw"
+          maxW="none"
+        >
+          <ModalCloseButton />
+          <ModalBody
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            p={0}
+          >
+            <Box w="full">
+              <Slider {...settings}>
+                {selectedImage?.subImgs.map((subImage) => (
+                  <Image
+                    borderRadius="md"
+                    p={{ base: 2, md: 2, lg: 1 }}
+                    src={subImage}
+                    w="auto"
+                    h="auto"
+                    maxH="100vh"
+                    maxW="100%"
+                  />
+                ))}
+              </Slider>
+            </Box>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </>
-  )
+  );
 };
