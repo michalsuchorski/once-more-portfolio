f422cf2967dbb193366c6aef34bb69742af71cac
Michal Suchorski <michal.suchorski7@gmail.com>
2024-07-17 12:34:26 +0200

    change grid gallery into a reusable component

--- src/components/ClothesGallery/index.tsx
+++ src/components/ClothesGallery/index.tsx
@@ -1,41 +1,25 @@
 import {
-  Box,
   Grid,
-  GridItem,
-  Image,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalOverlay,
   VStack,
-  useDisclosure,
 } from "@chakra-ui/react";
 import img1 from "../../pages/Marynarki/imgs/AC6336D8-F8E1-4FF3-B5E0-2A0A3454251E.png";
 import img2 from "../../pages/Marynarki/imgs/img2.png";
 import img3 from "../../pages/Marynarki/imgs/img3.png";
 import img4 from "../../pages/Marynarki/imgs/img4.png";
 
+import m1 from '../../pages/Marynarki/imgs/m1.png'
+import m2 from '../../pages/Marynarki/imgs/m2.png'
+import m3 from '../../pages/Marynarki/imgs/m3.png'
+import m4 from '../../pages/Marynarki/imgs/m4.png'
+
+
 import React from "react";
-import Slider from "react-slick";
-import { NextArrow, PrevArrow } from "../CarouselArrows";
+import { GalleryItem } from "../GalleryItem";
+import { ImageData } from "../GalleryItem/types";
 
 export const ClothesGallery: React.FC = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const [selectedImage, setSelectedImage] = React.useState<ImageData | null>(
-    null
-  );
 
-  const handleImageClick = (image: ImageData) => {
-    setSelectedImage(image);
-    onOpen();
-  };
 
-  interface ImageData {
-    src: string;
-    alt: string;
-    subImgs: Array<string>;
-  }
 
   const images: ImageData[] = [
     {
@@ -43,37 +27,12 @@ export const ClothesGallery: React.FC = () => {
       alt: "img1",
       subImgs: [img2, img3, img4],
     },
+    {
+        src: m1,
+        alt: 'img2',
+        subImgs: [m2, m3, m4]
+    }
   ];
-
-  const settings = {
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-    responsive: [
-        {
-            breakpoint: 1025,
-            settings: {
-                dots: true,
-                slidesToShow: 2,
-                slidesToScroll: 2,
-                infinite: true,
-                nextArrow: <NextArrow />,
-                prevArrow: <PrevArrow />,
-            },  
-        },
-        {
-            breakpoint: 644,
-            settings: {
-                dots: true,
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true,
-                nextArrow: <NextArrow />,
-                prevArrow: <PrevArrow />,
-            },
-        }
-    ]
-  };
 
   return (
     <>
@@ -83,49 +42,7 @@ export const ClothesGallery: React.FC = () => {
           w="full"
           gap={{ base: 2, md: 4, lg: 8 }}
         >
-          {images.map((image) => {
-            return (
-              <GridItem
-                w="100%"
-                cursor="pointer"
-                key={image.src}
-                onClick={() => handleImageClick(image)}
-              >
-                <Image src={image.src} />
-                <Modal isOpen={isOpen} onClose={onClose} isCentered>
-                  <ModalOverlay />
-                  <ModalContent height={{base: '75vh', md: '80vh', lg: '100vh'}} w="100vw" maxW="none">
-                    <ModalCloseButton />
-                    <ModalBody
-                      display="flex"
-                      flexDirection="column"
-                      justifyContent="center"
-                      alignItems="center"
-                      p={0}
-                    >
-                      <Box w="full">
-                        <Slider {...settings}>
-                          {selectedImage?.subImgs.map((subImage) => {
-                            return (
-                              <Image
-                                borderRadius='md'
-                                p={{ base: 2, md: 2, lg: 1}}
-                                src={subImage}
-                                w="auto"
-                                h="auto"
-                                maxH="100vh"
-                                maxW="100%"
-                              />
-                            );
-                          })}
-                        </Slider>
-                      </Box>
-                    </ModalBody>
-                  </ModalContent>
-                </Modal>
-              </GridItem>
-            );
-          })}
+          <GalleryItem images={images} />
         </Grid>
       </VStack>
     </>
---
+++ src/components/GalleryItem/index.tsx
@@ -0,0 +1,99 @@
+import { Box, GridItem, Image, Modal, ModalBody, ModalCloseButton, ModalContent, ModalOverlay, useDisclosure } from "@chakra-ui/react";
+import React from "react";
+import { NextArrow, PrevArrow } from "../CarouselArrows";
+import Slider from "react-slick";
+import { ImageData } from "./types";
+
+interface GalleryItemProps {
+    images: ImageData[]
+}
+
+export const GalleryItem: React.FC<GalleryItemProps> = ({ images }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [selectedImage, setSelectedImage] = React.useState<ImageData | null>(
+    null
+  );
+
+  const handleImageClick = (image: ImageData) => {
+    setSelectedImage(image);
+    onOpen();
+  };
+
+  const settings = {
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+        {
+            breakpoint: 1025,
+            settings: {
+                dots: true,
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                nextArrow: <NextArrow />,
+                prevArrow: <PrevArrow />,
+            },  
+        },
+        {
+            breakpoint: 644,
+            settings: {
+                dots: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                nextArrow: <NextArrow />,
+                prevArrow: <PrevArrow />,
+            },
+        }
+    ]
+  };
+
+  return (
+    <>
+         {images.map((image) => {
+            return (
+              <GridItem
+                w="100%"
+                cursor="pointer"
+                key={image.src}
+                onClick={() => handleImageClick(image)}
+              >
+                <Image src={image.src} />
+                <Modal isOpen={isOpen} onClose={onClose} isCentered>
+                  <ModalOverlay />
+                  <ModalContent height={{base: '85vh', md: '85vh', lg: '100vh'}} w="100vw" maxW="none">
+                    <ModalCloseButton />
+                    <ModalBody
+                      display="flex"
+                      flexDirection="column"
+                      justifyContent="center"
+                      alignItems="center"
+                      p={0}
+                    >
+                      <Box w="full">
+                        <Slider {...settings}>
+                          {selectedImage?.subImgs.map((subImage) => {
+                            return (
+                              <Image
+                                borderRadius='md'
+                                p={{ base: 2, md: 2, lg: 1}}
+                                src={subImage}
+                                w="auto"
+                                h="auto"
+                                maxH="100vh"
+                                maxW="100%"
+                              />
+                            );
+                          })}
+                        </Slider>
+                      </Box>
+                    </ModalBody>
+                  </ModalContent>
+                </Modal>
+              </GridItem>
+            );
+          })}
+    </>
+  )
+};
